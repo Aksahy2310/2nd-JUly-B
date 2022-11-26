ce0f4faa324ab4349b1c35b628ff4fd6630a659a
Akshay2310 <119030810+Aksahy2310@users.noreply.github.com>
2022-11-25 22:46:09 -0800

    Update myproject.java

--- src/Pkg1/myproject.java
+++ src/Pkg1/myproject.java
@@ -5,6 +5,12 @@ public class myproject {
 public static void main(String[] args) {
 	
 	System.out.println("india is my country");
+	
+	System.out.println("india is my country");
+	
+	System.out.println("india is my country");
+	
+	System.out.println("india is my country");
 		
 	}
 
